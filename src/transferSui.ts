0c23c1347638c881ebecb284660ab647c7cdeccb
tommy900122 <tommy900122@gmail.com>
2025-02-09 21:51:36 +0800

    add promise<string> for transfer sui

--- src/transferSui.ts
+++ src/transferSui.ts
@@ -1,16 +1,11 @@
-import { t, type Context } from "elysia"
-
-import { SuiClient, type CoinStruct } from '@mysten/sui/client';
+import { SuiClient } from '@mysten/sui/client';
 import { Transaction, type ObjectRef } from "@mysten/sui/transactions";
 import { isValidSuiAddress } from '@mysten/sui/utils'
-import { decodeSuiPrivateKey } from '@mysten/sui/cryptography';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
-import { ReponseError, ReponseOk } from "./response";
-
 import { getFullnodeUrl } from '@mysten/sui/client';
 
 
-export async function transferSui(account: string, amount: string) {
+export async function transferSui(account: string, amount: string): Promise<string> {
     let client = new SuiClient({ url: process.env.SUI_RPC || getFullnodeUrl("devnet") })
     let keypair = Ed25519Keypair.fromSecretKey(process.env.SUI_PRIVATEKEY || "",)
     let objectId = process.env.TOKEN_PROJECT_ID || ""
